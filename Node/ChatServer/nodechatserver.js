245041eab254f45f402591ffb3d235d58ffcbda2
nikonian <nikoniansgithub@outlook.com>
2014-11-26 23:50:55 +0000

    added indentations to make it more readable

--- Node/ChatServer/nodechatserver.js
+++ Node/ChatServer/nodechatserver.js
@@ -3,27 +3,28 @@ var net = require('net');
 var connections = [];
 
 var server = net.createServer(function (connection) {
-  connection.write('Welcome!\r\n');
-  connections.push(connection);
+	
+	connection.write('Welcome!\r\n');
+	connections.push(connection);
   
-  for(var i in connections)
-	{		
-	connections[i].write('*** There\'s ' + connections.length + ' participants in the room ***\r\n');
+	for(var i in connections) {		
+		connections[i].write('*** There\'s ' + connections.length + ' participants in the room ***\r\n');
 	}
 		
-  connection.on('data', function(data) {	
-	for(var i in connections)
-	{
-		if(i != connections.indexOf(connection))
-			connections[i].write(data);
-	}
-  });
+	connection.on('data', function(data) {	
+	
+		for(var i in connections) {
+			if(i != connections.indexOf(connection))
+				connections[i].write(data);
+		}
+    });
 
-  connection.on('end', function() {	
+	connection.on('end', function() {	
 		var x = connections.indexOf(connection);
 		connections.splice(x,1)
 			
 	for(var i in connections)
 		connections[i].write('*** There\'s ' + connections.length + ' participants in the room ***\r\n');
-  });    
+	});    
+	
 }).listen(8888);
